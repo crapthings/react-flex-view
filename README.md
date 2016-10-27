fa74409a3bd3d56eeff959894beec515ad005461
crapthings <crapthings@gmail.com>
2016-10-27 16:21:16 +0800

    update readme, add width support, x-scroll, flex grow...

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-flex-view
+
+this repo is inspired by
+
+> https://github.com/nachoaIvarez/flexbox-react
+> https://github.com/tcoopman/react-flexbox
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Flex = ({
   flexHeight,
   row,
   column,
+  flex,
   wrap,
   xEnd,
   xCenter,
@@ -20,6 +21,9 @@ const Flex = ({
   eight,
   nine,
   ten,
+  width,
+  height,
+  xScroll,
   yScroll,
   tag,
   style,
@@ -28,11 +32,12 @@ const Flex = ({
 }) => {
 
   const flexStyle = {
+    overflowX: `${xScroll && 'scroll'}`,
     overflowY: `${yScroll && 'scroll'}`,
     display: 'flex',
-    flex: `${(ten && 10) || (nine && 9) || (eight && 8) || (seven && 7) || (six && 6) || (five && 5) || (four && 4) || (three && 3) || (two && 2) || (one && 1) || ''}`,
-    width: '100%',
-    height: `${flexHeight ? '100vh' : 'auto'}`,
+    flex: `${(ten && 10) || (nine && 9) || (eight && 8) || (seven && 7) || (six && 6) || (five && 5) || (four && 4) || (three && 3) || (two && 2) || (one && 1) || flex || ''}`,
+    width: `${width || '100%'}`,
+    height: `${flexHeight ? '100vh' : (height || 'auto')}`,
     flexFlow: `${column ? 'column' : 'row'} ${wrap ? 'wrap' : 'nowrap'}`,
     justifyContent: `${(column && yEnd && 'flex-end') || (column && yCenter && 'center') || (!column && xEnd && 'flex-end') || (!column && xCenter && 'center') || (center && 'center')}`,
     alignItems: `${(column && xEnd && 'flex-end') || (column && xCenter && 'center') || (!column && yEnd && 'flex-end') || (!column && yCenter && 'center') || (center && 'center')}`,
