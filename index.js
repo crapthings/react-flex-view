839b53df4ec1c8385eb1b0567e8d48a99d329b54
crapthings <crapthings@gmail.com>
2016-10-28 14:40:33 +0800

    change pattern

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Flex = ({
+const FlexView = ({
   flexHeight,
   row,
   column,
@@ -23,9 +23,14 @@ const Flex = ({
   ten,
   width,
   height,
+  fitWidth,
+  fitHeight,
+  pdlr,
   scroll,
   xScroll,
   yScroll,
+  bgColor,
+  color,
   element,
   tag,
   style,
@@ -33,21 +38,26 @@ const Flex = ({
   ...props,
 }) => {
 
-  const flexStyle = {
+  const FlexStyle = {
     overflowX: `${xScroll && 'scroll'}`,
     overflowY: `${(scroll || yScroll) && 'scroll'}`,
     display: 'flex',
     flex: `${(ten && 10) || (nine && 9) || (eight && 8) || (seven && 7) || (six && 6) || (five && 5) || (four && 4) || (three && 3) || (two && 2) || (one && 1) || flex || ''}`,
-    width: `${width || '100%'}`,
-    height: `${flexHeight ? '100vh' : (height || 'auto')}`,
+    width: `${width}`,
+    height: `${flexHeight ? '100vh' : (height)}`,
+    padding: ``,
+    paddingLeft: `${pdlr && '8px'}`,
+    paddingRight: `${pdlr && '8px'}`,
     flexFlow: `${column ? 'column' : 'row'} ${wrap ? 'wrap' : 'nowrap'}`,
     justifyContent: `${(column && yEnd && 'flex-end') || (column && yCenter && 'center') || (!column && xEnd && 'flex-end') || (!column && xCenter && 'center') || (center && 'center')}`,
     alignItems: `${(column && xEnd && 'flex-end') || (column && xCenter && 'center') || (!column && yEnd && 'flex-end') || (!column && yCenter && 'center') || (center && 'center')}`,
+    backgroundColor: `${bgColor}`,
+    color: `${color}`,
     ...style
   }
 
-  return React.createElement(element || tag || 'div', { style: flexStyle, ...props }, children)
+  return React.createElement(element || tag || 'div', { style: FlexStyle, ...props }, children)
 
 }
 
-export default Flex
+export default FlexView
